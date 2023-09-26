42f5dced51671f3365d3fcb8d584788c01eeee0c
Kadiraydn01 <kadiraydn01@gmail.com>
2023-09-26 13:54:23 +0300

    All Classes Done

--- src/com/workintech/Home/Bed.java
+++ src/com/workintech/Home/Bed.java
@@ -1,34 +1,37 @@
 package com.workintech.Home;
 
 public class Bed {
-    private String pillows;
-    private String height;
-    private String sheets;
-    private String quilt;
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilt;
 
-    public Bed(String pillows, String height, String sheets, String quilt) {
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        this.style = style;
         this.pillows = pillows;
         this.height = height;
         this.sheets = sheets;
         this.quilt = quilt;
     }
-    public void make(){
-        System.out.println("The bed is being made");
-    }
 
-    public String getPillows() {
+    public int getPillows() {
         return pillows;
     }
 
-    public String getHeight() {
+    public int getHeight() {
         return height;
     }
 
-    public String getSheets() {
+    public int getSheets() {
         return sheets;
     }
 
-    public String getQuilt() {
+    public int getQuilt() {
         return quilt;
+    }
+
+    public void make() {
+        System.out.println("The bed is being made");
     }
 }
